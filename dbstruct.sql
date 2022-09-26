8653878d37e9dd63892b4daa92ed180458ed2dff
sharpstorm <chimchar17@gmail.com>
2022-09-26 19:34:25 +0800

    chore: remove notion of user

--- dbstruct.sql
+++ dbstruct.sql
@@ -1,9 +1,3 @@
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    user_name VARCHAR(255) UNIQUE,
-    user_password VARCHAR(255)
-);
-
 CREATE TABLE api_tokens (
 	token_id SERIAL PRIMARY KEY,
     token VARCHAR(255) UNIQUE,
