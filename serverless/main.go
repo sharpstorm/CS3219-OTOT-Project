10cbbc62c27809e72941f891ee2e4cbc3f66ddd0
sharpstorm <chimchar17@gmail.com>
2022-10-20 01:32:16 +0800

    feat: enable CORS on serverless function

--- serverless/main.go
+++ serverless/main.go
@@ -49,6 +49,16 @@ func GetPrice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	if r.Method != http.MethodPost {
 		writeError(w, 405, "Only POST is allowed")
 		return
